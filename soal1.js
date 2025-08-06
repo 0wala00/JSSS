24e26f25bb95ef1fe851523018fffc2f577a1a27
Akhdan Prajadisa <akhdanprajadisa962@gmail.com>
2025-08-06 20:29:37 +0700

    Update v3

--- soal1.js
+++ soal1.js
@@ -4,3 +4,7 @@ const prompt = require("prompt-sync")({ sigint: true });
 console.log("**********************************");
 console.log("UJIAN ALGORITMA AKHDAN");
 console.log("-------------------------------------------------------");
+
+
+console.log("**********************************");
+console.log("UJIAN JAMAL KUKURUYUK");
