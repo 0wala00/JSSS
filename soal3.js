a65353c6429c0c0c13d984c9768ba65cbd9219e3
Akhdan Prajadisa <akhdanprajadisa962@gmail.com>
2025-08-07 10:18:35 +0700

    praBeres

--- soal3.js
+++ soal3.js
@@ -2,14 +2,31 @@
 //soal no 3
 
 
+const prompt = require("prompt-sync")({ sigint: true });
+
+a = prompt("ketik c untuk console, ketik w untuk web: ");
+
+
+
 let addres = "Cianjur";
 let kouta = 258;
 
+
+
+
+let a = "c, w";
+let pilihan;
+
+switch (a) {
+  case c:
 console.log(addres);
 console.log(kouta);
-
-// Tampilkan data ke HTML
+    break;
+  case w:
 document.body.innerHTML = `
 adres: ${addres}<br>
 kouta: ${kouta}
 `; 
+    break;
+}
+ // Output: Selasa
---
+++ soal7.js
@@ -0,0 +1,18 @@
+let a , b, c, d, e, hasilTambah, hasilKurang, hasilKali, hasilBagi;
+a = 8.3
+b = "3.2";
+c = parseFloat(b);
+ // hasil: 5
+
+
+hasilTambah = a + c 
+hasilKurang = a - c 
+hasilKali = a * c
+
+  
+
+console.log("Hasil Tambah" ,+ hasilTambah.toFixed(2)); 
+console.log("Hasil kurang" ,+ hasilKurang.toFixed(2)); 
+console.log("Hasil Kali" ,+ hasilKali.toFixed(2)); 
+c = parseInt(b);
+console.log("Hasil Bagi" ,+ hasilBagi); 
