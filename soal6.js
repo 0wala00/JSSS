3d4c6fd9dac5f8fee5a9e19422e582b1d6f8edd9
Akhdan Prajadisa <akhdanprajadisa962@gmail.com>
2025-08-17 23:05:03 +0700

    Update v3

--- soal6.js
+++ soal6.js
@@ -2,11 +2,23 @@
 // Akhdan X-PPLG-2
 
 const prompt = require("prompt-sync")({ sigint: true })
-let nama, nilai, kelulusan
-nama = parseInt(prompt(`Nama : `))
-nilai = parseInt(prompt(`Nilai rapot : `))
-if (nilai >= `85`) {
-    console.log(`Lulus`)
+let nama, nilai, kelulusan, sepatu;
+nama = prompt("Nama : ");
+sepatu = parseInt(prompt("Ukuran sepatu : "));
+nilai = parseFloat(prompt("Nilai rapot : "));
+
+console.log("____________________OUTPUT______________________")
+console.log("Nama: " + nama);
+console.log("Ukuran Sepatu: " + sepatu);
+console.log("Nilai Raport: " + nilai);
+
+if (nilai >= 85) {
+    console.log("Kelulusan: " + true);
 } else {
-    console.log(`Tidak lulus`)
+    console.log("Kelulusan: " + false);
 }
+
+
+  document.body.innerHTML = `
+<P>"Halo"<p/>
+      `;
